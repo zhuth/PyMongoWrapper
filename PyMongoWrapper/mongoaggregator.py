536d4214d1d12e2cb0113be45ed529e6bd359094
zhuth <zthpublic@gmail.com>
2021-07-07 10:32:28 +0800

    aggregator update

--- PyMongoWrapper/mongoaggregator.py
+++ PyMongoWrapper/mongoaggregator.py
@@ -15,17 +15,24 @@ class MongoAggregatingFunction:
 
 class MongoAggregator:
 
-    def __init__(self, performer=None, aggregators=None):
+    def __init__(self, performer=None, aggregators=None, raw=False):
         self.performer = performer
+        self.raw = raw
         self.aggregators = aggregators or list()
 
     def __getattr__(self, name):
         return MongoAggregatingFunction(name, self)
 
-    def perform(self, raw=False, performer=None):
-        performer = performer or self.performer
-        assert performer, 'Must assign a performer'
-        return performer.aggregate(self.aggregators, raw=raw, allowDiskUse=True)
+    def __iter__(self):
+        assert self.performer, 'Must assign a performer'
+        for i in self.performer.aggregate(self.aggregators, raw=self.raw, allowDiskUse=True):
+            yield i
+
+    def perform(self, performer=None, raw=False):
+        if performer:
+            self.performer = performer
+        self.raw = raw
+        return self
 
     def count(self):
         for a in self.group(_id=1, count={'$sum': 1}).perform():
