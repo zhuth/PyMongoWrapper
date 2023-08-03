c8463de820596098966ccccdef8df7723a24dd76
zhuth <thzhu@139.com>
2023-08-03 13:01:31 +0800

    first() returns none when not matched

--- PyMongoWrapper/mongoresultset.py
+++ PyMongoWrapper/mongoresultset.py
@@ -122,7 +122,6 @@ class MongoResultSet:
         """
         for r in self:
             return r
-        # return self.ele_cls()
 
     def update(self, updt, **update_kwargs):
         """Perform update on current result set.
